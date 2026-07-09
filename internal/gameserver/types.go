660c215c6826571a1137ca7d84c762f9f9d07346
coffeebased <107894874+coffeebased@users.noreply.github.com>
2026-07-09 14:57:08 -0300

    normalization and validation for requests

--- internal/gameserver/types.go
+++ internal/gameserver/types.go
@@ -1,6 +1,8 @@
 package gameserver
 
 import (
+	"errors"
+	"fmt"
 	"net"
 	"strconv"
 	"strings"
@@ -40,36 +42,158 @@ func (s Status) valid() bool {
 }
 
 type CreateRequest struct {
-	Code         string
-	Host         string
-	Port         int
-	QueryHost    string
-	QueryPort    int
-	Kind         Kind
-	Password     string
-	DisplaysIP   bool
-	DisplaysPort bool
-	Application  string
-	Version      string
-	MaxPlayers   int
-	Title        string
-	Description  string
+	Code             string
+	Host             string
+	Port             int
+	QueryHost        string
+	QueryPort        int
+	Kind             Kind
+	Password         string
+	DisplaysIP       bool
+	DisplaysPort     bool
+	Ignore           bool
+	OverrideDefaults bool
+	Application      string
+	Version          string
+	MaxPlayers       int
+	Title            string
+	Description      string
+}
+
+func (r *CreateRequest) Normalize() {
+	r.Code = strings.ToUpper(strings.TrimSpace(r.Code))
+	r.Host = strings.TrimSpace(r.Host)
+	r.QueryHost = strings.TrimSpace(r.QueryHost)
+
+	if r.Kind == "" {
+		r.Kind = KindNone
+	}
+
+	r.Password = strings.TrimSpace(r.Password)
+	r.Application = strings.TrimSpace(r.Application)
+	r.Version = strings.TrimSpace(r.Version)
+	r.Title = strings.TrimSpace(r.Title)
+	r.Description = strings.TrimSpace(r.Description)
+}
+
+func (r CreateRequest) Validate() error {
+	if r.Code == "" {
+		return errors.New("code is required")
+	}
+
+	if r.Code != strings.ToUpper(r.Code) {
+		return errors.New("code must be upper case")
+	}
+
+	if r.Host == "" {
+		return errors.New("host is required")
+	}
+
+	if r.Port <= 0 || r.Port > 65535 {
+		return errors.New("port must be between 1 and 65535")
+	}
+
+	if r.QueryPort < 0 || r.QueryPort > 65535 {
+		return errors.New("query port must be between 0 and 65535")
+	}
+
+	if !r.Kind.valid() {
+		return fmt.Errorf("invalid kind: %s", r.Kind)
+	}
+
+	if r.QueryHost != "" && r.QueryPort == 0 {
+		return errors.New("query port must be provided along query host")
+	}
+
+	return nil
 }
 
 type UpdateRequest struct {
-	Host         *string
-	Port         *int
-	QueryHost    *string
-	QueryPort    *int
-	Kind         *Kind
-	Password     *string
-	DisplaysIP   *bool
-	DisplaysPort *bool
-	Application  *string
-	Version      *string
-	MaxPlayers   *int
-	Title        *string
-	Description  *string
+	Host             *string
+	Port             *int
+	QueryHost        *string
+	QueryPort        *int
+	Kind             *Kind
+	Password         *string
+	DisplaysIP       *bool
+	DisplaysPort     *bool
+	Ignore           *bool
+	OverrideDefaults *bool
+	Application      *string
+	Version          *string
+	MaxPlayers       *int
+	Title            *string
+	Description      *string
+}
+
+func (r *UpdateRequest) Normalize() {
+	if r.Host != nil {
+		host := strings.ToUpper(strings.TrimSpace(*r.Host))
+		r.Host = &host
+	}
+
+	if r.QueryHost != nil {
+		queryHost := strings.ToUpper(strings.TrimSpace(*r.QueryHost))
+		r.QueryHost = &queryHost
+	}
+
+	if r.Kind != nil && *r.Kind == "" {
+		kindNone := KindNone
+		r.Kind = &kindNone
+	}
+
+	if r.Password != nil {
+		password := strings.ToUpper(strings.TrimSpace(*r.Password))
+		r.Password = &password
+	}
+
+	if r.Application != nil {
+		application := strings.ToUpper(strings.TrimSpace(*r.Application))
+		r.Application = &application
+	}
+
+	if r.Version != nil {
+		version := strings.ToUpper(strings.TrimSpace(*r.Version))
+		r.Version = &version
+	}
+
+	if r.Title != nil {
+		title := strings.ToUpper(strings.TrimSpace(*r.Title))
+		r.Title = &title
+	}
+
+	if r.Description != nil {
+		description := strings.ToUpper(strings.TrimSpace(*r.Description))
+		r.Description = &description
+	}
+}
+
+func (r UpdateRequest) Validate() error {
+	if r.Host != nil && *r.Host == "" {
+		return errors.New("Host is required")
+	}
+
+	if r.Port != nil && (*r.Port <= 0 || *r.Port > 65535) {
+		return errors.New("port must be between 1 and 65535")
+	}
+
+	if r.QueryPort != nil && (*r.QueryPort < 0 || *r.QueryPort > 65535) {
+		return errors.New("query port must be between 0 and 65535")
+	}
+
+	if r.Kind != nil && !r.Kind.valid() {
+		return fmt.Errorf("invalid kind: %s", *r.Kind)
+	}
+
+	if r.QueryHost != nil && *r.QueryHost != "" && (r.QueryPort == nil || *r.QueryPort == 0) {
+		return errors.New("query port must be provided along query host")
+	}
+
+	if r.QueryPort != nil && *r.QueryPort == 0 && (r.QueryHost == nil || *r.QueryHost != "") {
+		return errors.New("query host must be set to empty string when query port is set to 0")
+	}
+
+	return nil
 }
 
 type Defaults struct {
@@ -94,17 +218,19 @@ type State struct {
 }
 
 type GameServer struct {
-	Code         string
-	Host         string
-	Port         int
-	QueryHost    string
-	QueryPort    int
-	Kind         Kind
-	Password     string
-	DisplaysIP   bool
-	DisplaysPort bool
-	Defaults     Defaults
-	State        State
+	Code             string
+	Host             string
+	Port             int
+	QueryHost        string
+	QueryPort        int
+	Kind             Kind
+	Password         string
+	DisplaysIP       bool
+	DisplaysPort     bool
+	Ignore           bool
+	OverrideDefaults bool
+	Defaults         Defaults
+	State            State
 }
 
 func (gs *GameServer) GetDisplayAddress() string {
